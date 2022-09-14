b88d4cb4ab11bc9652ec1ac5b7f6f91b54494bf4
MisterSynergy <mister.synergy@yahoo.com>
2022-09-14 11:06:12 +0200

    normalize folder structure

--- tool-init.sh
+++ tool-init.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-# make a folder for the sql backups
-mkdir ${HOME}/backup
-
 # clone repo and evaluate commit properties
-#git clone --recursive https://bitbucket.org/MisterSynergy/wikilaeum.git # this needs to be done manually before
+#git clone https://github.com/MisterSynergy/wikilaeum.git # this needs to be done manually before
 cd ${HOME}/wikilaeum
-git rev-list --format=format:'%ai' --max-count=1 `git rev-parse HEAD` > ${HOME}/wikilaeum/commit_version_and_timestamp.txt
+git rev-list --format=format:'%ai' --max-count=1 `git rev-parse HEAD` > ${HOME}/commit_version_and_timestamp.txt
 
 # copy configuration and put user's database user name into it
-if [ ! -f ${HOME}/wikilaeum/config.ini ]; then
-    cp ${HOME}/wikilaeum/config-sample.ini ${HOME}/wikilaeum/config.ini
+if [ ! -f ${HOME}/config.ini ]; then
+    cp ${HOME}/config-sample.ini ${HOME}/config.ini
     dbuser=`sed -n -e '/user/{s/.*= *//p}' ${HOME}/replica.my.cnf`
-    sed -i -- s/{}/${dbuser}/g ${HOME}/wikilaeum/config.ini
+    sed -i -- s/{}/${dbuser}/g ${HOME}/config.ini
 fi
 
-# create symbolic links in the home directory
-ln -isf ${HOME}/wikilaeum/public_html/ ${HOME}
-ln -isf ${HOME}/wikilaeum/tool-update.sh ${HOME}
-
 # database setup
-. ${HOME}/wikilaeum/config.ini
+. ${HOME}/config.ini
 mysql --defaults-file=${HOME}/replica.my.cnf -h ${tooldb_host} "CREATE DATABASE IF NOT EXISTS ${tooldb_dbname} DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
-mysql --defaults-file=${HOME}/replica.my.cnf -h ${tooldb_host} ${tooldb_dbname} < ${HOME}/wikilaeum/wikilaeum.sql
+mysql --defaults-file=${HOME}/replica.my.cnf -h ${tooldb_host} ${tooldb_dbname} < ${HOME}/wikilaeum.sql
 
 # create cron jobs
-(crontab -l ; echo "15 3 * * 1 sh ${HOME}/wikilaeum/db_backup.sh") | crontab -
-(crontab -l ; echo "20 3 * * */1 python3 ${HOME}/wikilaeum/daily_update.py") | crontab -
+(crontab -l ; echo "15 3 * * 1 sh ${HOME}/db_backup.sh") | crontab -
+(crontab -l ; echo "20 3 * * */1 python3 ${HOME}/daily_update.py") | crontab -
--- tool-update.sh
+++ tool-update.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-updatetime=$(date +"%Y%m%d%H%M%S")
-mkdir ${HOME}/wikilaeum-${updatetime}
-cp -a ${HOME}/wikilaeum/* ${HOME}/wikilaeum-${updatetime}
-cd ${HOME}/wikilaeum
+cd ${HOME}
 git pull --all
 git rev-list --format=format:'%ai' --max-count=1 `git rev-parse HEAD` > ${HOME}/commit_version_and_timestamp.txt

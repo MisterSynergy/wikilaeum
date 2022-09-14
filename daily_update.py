fd8c96391a9b34508d6d37896bb30e22808bb7dd
MisterSynergy <mister.synergy@yahoo.com>
2022-09-14 12:25:19 +0200

    modernize daily update script

--- daily_update.py
+++ daily_update.py
@@ -1,12 +1,34 @@
-#!/usr/bin/env python
 from os.path import expanduser
 from time import gmtime, strftime, time
 from configparser import ConfigParser
-from itertools import chain
+from typing import Optional
 
 from mysql.connector import MySQLConnection
 from mysql.connector.cursor import MySQLCursor
 from mysql.connector.errors import InterfaceError
+
+
+class Config:
+    def __init__(self):
+        self.config = ConfigParser(delimiters='=')
+
+        with open(
+            f'{expanduser("~")}/config.ini',
+            mode='r',
+            encoding='utf8'
+        ) as file_handle:
+            config_str = file_handle.read()
+        
+        self.config.read_string(f'[wikilaeum]\n{config_str}')
+
+        for key in self.config['wikilaeum']:
+            self.config['wikilaeum'][key] = self.config['wikilaeum'][key].replace('"', '')
+
+    def get_config(self, key:str) -> str:
+        if key not in self.config['wikilaeum']:
+            return ''
+
+        return self.config['wikilaeum'][key]
 
 
 class DatabaseCursor:
@@ -14,17 +36,13 @@ class DatabaseCursor:
         self.connection = MySQLConnection(
             host=host,
             database=database,
-            option_files=expanduser('~') + '/replica.my.cnf',
-            charset='utf8mb4',
-            collation='utf8mb4_bin',
+            option_files=f'{expanduser("~")}/replica.my.cnf',
             autocommit=autocommit
         )
         self.cursor = self.connection.cursor()
 
-
-    def __enter__(self):
+    def __enter__(self) -> MySQLCursor:
         return self.cursor
-
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.cursor.close()
@@ -32,81 +50,70 @@ class DatabaseCursor:
 
 
 class ReplicaCursor(DatabaseCursor):
-    def __init__(self, host:str, database:str) -> None:
+    def __init__(self) -> None:
         super().__init__(
-            host=host,
-            database=database
+            host=CONFIG.get_config('replica_dewiki_analytics_host'),
+            database=CONFIG.get_config('replica_dewiki_dbname')
         )
 
 
 class ToolDatabaseCursor(DatabaseCursor):
-    def __init__(self, host:str, database:str) -> None:
+    def __init__(self) -> None:
         super().__init__(
-            host=host,
-            database=database,
+            host=CONFIG.get_config('tooldb_host'),
+            database=CONFIG.get_config('tooldb_dbname'),
             autocommit=True
         )
 
 
-def query_mediawiki(host:str, database:str, query:str) -> list:
-    with ReplicaCursor(host, database) as cursor:
+def query_mediawiki(query:str) -> list[tuple]:
+    with ReplicaCursor() as cursor:
         cursor.execute(query)
         result = cursor.fetchall()
 
     return result
 
 
-def query_tool_database(host:str, database:str, query:str) -> list:
-    with ToolDatabaseCursor(host, database) as cursor:
+def query_tool_database(query:str) -> list[tuple]:
+    with ToolDatabaseCursor() as cursor:
         cursor.execute(query)
         result = cursor.fetchall()
 
     return result
 
 
-def read_config() -> ConfigParser: # TODO: tidy
-    config = ConfigParser()
-
-    #config.read(expanduser('~') + '/wikilaeum/config.ini')
-    with open(expanduser('~') + '/wikilaeum/config.ini', mode='r', encoding='utf8') as lines:
-        lines = chain(('[wikilaeum]',), lines)
-        config.read_file(lines)
-
-    for key in config['wikilaeum']:
-        config['wikilaeum'][key] = config['wikilaeum'][key].replace('"', '')
-
-    return config
-
-
-def query_new_heavyusers(host:str, database:str, user_ids:list, limit:int) -> list:
+def query_new_heavyusers(user_ids:list[int]) -> dict[int, str]:
     time_start = time()
 
-    print('Perform database query to cache data about up to {:d} users' \
-          ' (this may take a while) ...'.format(limit))
+    print(f'Perform database query to cache data about {len(user_ids):d} users' \
+          ' (this may take a while) ...')
 
-    query = """SELECT
+    query = f"""SELECT
   actor_user,
   MIN(rev_timestamp) AS first_edit
 FROM
   revision_userindex
     JOIN actor_revision ON rev_actor=actor_id
 WHERE
-  actor_user IN ({user_ids_concatenated})
+  actor_user IN ({",".join([ str(user_id) for user_id in user_ids ])})
 GROUP BY
-  actor_user""".format(user_ids_concatenated=','.join([ str(user_id) for user_id in user_ids ]))
+  actor_user"""
 
-    result = query_mediawiki(host, database, query)
-
-    print('This took {duration:d} seconds'.format(duration=int(time()-time_start)))
+    query_result = query_mediawiki(query)
+    print(f'This took {int(time()-time_start):d} seconds')
+    
+    result = {}
+    for tpl in query_result:
+        result[tpl[0]] = tpl[1].decode('utf8')
 
     return result
 
 
-def insert_user_id(cursor:MySQLCursor, user_id:str, first_edit:str) -> int:
-    query = """INSERT INTO
+def insert_user_id(cursor:MySQLCursor, user_id:int, first_edit:str) -> int:
+    query = f"""INSERT INTO
   localuser (user_id, timestmp_first)
 VALUES
-  ({user_id}, {first_edit})""".format(user_id=user_id, first_edit=first_edit)
+  ({user_id:d}, {first_edit})"""
 
     try:
         cursor.execute(query)
@@ -117,68 +124,60 @@ VALUES
     return 1
 
 
-def insert_user_ids(replica_host:str, replica_database:str, tool_host:str, tool_db:str, \
-                    user_ids:list, limit:int) -> int:
+def insert_user_ids(user_ids:list[int]) -> int:
     if len(user_ids) == 0:
         return 0
 
-    new_heavyusers = query_new_heavyusers(replica_host, replica_database, user_ids, limit)
+    new_heavyusers = query_new_heavyusers(user_ids)
 
     added_cnt = 0
-    with ToolDatabaseCursor(tool_host, tool_db) as cursor:
-        for tpl in new_heavyusers:
-            user_id = str(tpl[0])
-            first_edit = tpl[1].decode('utf8')
-
-            print('Inserting now ({added_cnt:3d}/{limit:3d}): {user_id} --- {first_edit}'.format(
-                added_cnt=added_cnt+1,
-                limit=limit,
-                user_id=user_id,
-                first_edit=first_edit
-            ))
+    with ToolDatabaseCursor() as cursor:
+        for user_id, first_edit in new_heavyusers.items():
+            print(f'Inserting now ({added_cnt+1:3d}/{len(user_ids):3d}):' \
+                  f' {user_id:d} --- {first_edit}')
 
             added_cnt += insert_user_id(cursor, user_id, first_edit)
 
     return added_cnt
 
 
-def query_heavyusers(host:str, database:str, limit:int) -> list:
-    query = """SELECT user_id FROM user WHERE user_editcount>={limit}""".format(limit=limit)
-    result = query_mediawiki(host, database, query)
+def query_heavyusers(limit:int) -> list[int]:
+    query = f"""SELECT user_id FROM user WHERE user_editcount>={limit}"""
+    query_result = query_mediawiki(query)
 
-    print('Found {cnt:d} users with more than {limit:d} edits in replica database'.format(
-        cnt=len(result),
-        limit=limit
-    ))
+    user_ids = [ user_id_tuple[0] for user_id_tuple in query_result ]
 
-    return result
+    print(f'Found {len(user_ids):d} users with more than {limit:d} edits in replica database')
+
+    return user_ids
 
 
-def query_localusers(host:str, database:str, ) -> list:
+def query_localusers() -> list[int]:
     query = 'SELECT user_id FROM localuser'
-    result = query_tool_database(host, database, query)
+    query_result = query_tool_database(query)
 
-    print('Found {cnt:d} users in local database'.format(cnt=len(result)))
+    user_ids = [ user_id_tuple[0] for user_id_tuple in query_result ]
 
-    return result
+    print(f'Found {len(user_ids):d} users in local database')
+
+    return user_ids
 
 
-def evaluate_user_ids_to_update(heavyusers:list, localusers:list, limit:int) -> list:
-    user_ids = [ int(user_id[0]) for user_id in heavyusers if user_id not in localusers ]
-    print('{cnt:d} users need to be added'.format(cnt=len(user_ids)))
+def evaluate_user_ids_to_update(heavyusers:list[int], localusers:list[int], \
+                                limit:Optional[int]=None) -> list[int]:
+    user_ids = [ user_id for user_id in heavyusers if user_id not in localusers ]
 
-    if limit < len(user_ids):
+    print(f'{len(user_ids):d} users need to be added')
+
+    if limit is not None and limit < len(user_ids):
         user_ids = user_ids[:limit]
-        print('{cnt:d} users will be added (set by limit of {limit})'.format(
-            cnt=len(user_ids),
-            limit=limit
-        ))
+        print(f'{len(user_ids):d} users will be added (set by limit of {limit})')
 
     return user_ids
 
 
 def make_output(output:str) -> None:
-    with open('./daily_update_log.txt', mode='w', encoding='utf8') as file_handle:
+    with open(f'{expanduser("~")}/daily_update_log.txt', mode='w', encoding='utf8') as file_handle:
         file_handle.write(output)
 
     print(output)
@@ -187,46 +186,27 @@ def make_output(output:str) -> None:
 def main() -> None:
     # init
     now_at_start = time()
-    config = read_config()
-
-    editcount_threshold = int(config['wikilaeum']['editcount_threshold'])
-    insert_limit = int(config['wikilaeum']['insertcounter_limit'])
+    
+    editcount_threshold = int(CONFIG.get_config('editcount_threshold'))
+    insert_limit = int(CONFIG.get_config('insertcounter_limit'))
 
     # read database entries
-    heavyusers = query_heavyusers(
-        config['wikilaeum']['replica_dewiki_analytics_host'],
-        config['wikilaeum']['replica_dewiki_dbname'],
-        editcount_threshold
-    )
-    localusers = query_localusers(
-        config['wikilaeum']['tooldb_host'],
-        config['wikilaeum']['tooldb_dbname']
-    )
+    heavyusers = query_heavyusers(editcount_threshold)
+    localusers = query_localusers()
 
     # prepare update
-    user_ids = evaluate_user_ids_to_update(heavyusers, localusers, insert_limit)
+    user_ids = evaluate_user_ids_to_update(heavyusers, localusers, limit=insert_limit)
 
     # perform update
-    added_cnt = insert_user_ids(
-        config['wikilaeum']['replica_dewiki_analytics_host'],
-        config['wikilaeum']['replica_dewiki_dbname'],
-        config['wikilaeum']['tooldb_host'],
-        config['wikilaeum']['tooldb_dbname'],
-        user_ids,
-        insert_limit
-    )
+    added_cnt = insert_user_ids(user_ids)
 
-    make_output('{timestmp} UTC (editcount_threshold={editcount_threshold:d}):' \
-        ' {cnt_heavyusers:d} users in dewiki, {cnt_localusers:d} users locally,' \
-        ' {added_cnt:d} users added; update execution time: {time_consumed:.2f}' \
-        ' seconds'.format(
-        timestmp=strftime('%Y-%m-%d %H:%M:%S', gmtime()),
-        editcount_threshold=editcount_threshold,
-        cnt_heavyusers=len(heavyusers),
-        cnt_localusers=len(localusers),
-        added_cnt=added_cnt,
-        time_consumed=time()-now_at_start
-    ))
+    make_output(f'{strftime("%Y-%m-%d %H:%M:%S", gmtime())} UTC (editcount_threshold=' \
+        f'{editcount_threshold:d}): {len(heavyusers):d} users in dewiki, {len(localusers):d} ' \
+        f'users locally, {added_cnt:d} users added; update execution time: ' \
+        f'{time()-now_at_start:.2f} seconds')
+
+
+CONFIG = Config()
 
 
 if __name__=='__main__':

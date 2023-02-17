efed3d108aae7644b28f0e97083058310630ae7e
MisterSynergy <mister.synergy@yahoo.com>
2023-02-17 13:44:02 +0100

    file permissions after update

--- tool-update.sh
+++ tool-update.sh
@@ -2,3 +2,6 @@
 cd ${HOME}
 git pull --all
 git rev-list --format=format:'%ai' --max-count=1 `git rev-parse HEAD` > ${HOME}/commit_version_and_timestamp.txt
+
+# file permissions
+chmod u+x ${HOME}/*.sh
